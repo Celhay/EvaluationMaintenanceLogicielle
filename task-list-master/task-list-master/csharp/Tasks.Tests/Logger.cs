819c9bc70e2745f796ccb666f16ae76e12abbb3b
Celhay <>
2020-06-29 17:20:39 +0200

    Ajout class logger
    Suivit d'un tuto pour pouvoir lire les entrées et sorties Consoles

---
+++ task-list-master/task-list-master/csharp/Tasks.Tests/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Tasks.Tests
+{
+    public class Logger : IDisposable
+    {
+        private StringWriter stringWriter;
+        private TextWriter textWriter;
+        public string Get()
+        {
+            string
+        }
+        public void Dispose()
+        {
+            Console.SetOut(originalOutput);
+        }
+    }
+}
